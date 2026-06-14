e2bf59b563e3c02a9f61a4a92844a71cad068bd0
relizanedoc <sisi.sou4884@gmail.com>
2026-06-14 14:12:18 +0100

    Update Google Maps iframe source URL

--- script.js
+++ script.js
@@ -828,7 +828,7 @@ function openDoctorProfileModal(doc, doctorName) {
           
           <div style="border-radius: 12px; overflow: hidden; border: 1px solid var(--border); margin-bottom: ${doc.map_link ? '1rem' : '0'}; position: relative; padding-bottom: 65%; height: 0; background: #f8fafc;">
            <iframe 
-src="http://googleusercontent.com/maps.google.com/${encodeURIComponent(doc.exact_location + ', ' + t(doc.municipality) + ', Relizane')}&t=&z=15&ie=UTF8&iwloc=&output=embed"
+    src="https://maps.google.com/maps?q=${encodeURIComponent(doc.exact_location + ' ' + t(doc.municipality) + ' Relizane')}&t=&z=15&ie=UTF8&iwloc=&output=embed" 
     style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;" 
     allowfullscreen="" 
     loading="lazy" 
